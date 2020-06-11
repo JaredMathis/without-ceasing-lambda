3b21092f416dd7999ad32427676531626c0c07b3
JaredMathis <jaredmathis@protonmail.com>
2020-06-11 15:36:56 -0400

    checkpoint

--- library/wcRequestPrayer.js
+++ library/wcRequestPrayer.js
@@ -3,6 +3,7 @@ const u = require("wlj-utilities");
 const lib = require("without-ceasing-library");
 const aws = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
+const getPrayerRequests = require('./getPrayerRequests');
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
@@ -12,6 +13,17 @@ async function wcRequestPrayer(event, context, callback) {
     await u.awsScope(async (x) => {
 
         let request = lib.toRequest(event);
+
+        let requests = await getPrayerRequests(x);
+        for (let r of requests) {
+            if (u.propertiesAreEqual(
+                r.request,
+                request,
+                ['userId', 'petitionId', 'nameId']
+            )) {
+                return 'Already requested!'
+            }
+        }
 
         await requestPrayer(x, request);
 
@@ -27,18 +39,18 @@ async function requestPrayer(x, request) {
     let date = `${now.getFullYear()}/${month}/${day}`;
 
     let time = `${now.getHours()}/${now.getMinutes()}`;
-    
+
     let eventId = uuidv4();
     let key = `${date}/${time}/${eventId}`;
-    u.merge(x, {key});
+    u.merge(x, { key });
     const params = {
-        Bucket:  "without-ceasing-data",
+        Bucket: "without-ceasing-data",
         Key: key,
         Body: JSON.stringify({
             request,
         }),
     };
-    u.merge(x, {params});
+    u.merge(x, { params });
 
     const response = await s3.upload(params).promise();
     return { key };
